e278978834969f6bd874f753916728ec626b276a
asadali27232 <asadali27232@gmail.com>
2023-03-27 06:08:12 +0500

    Updated

--- Lab1/task1.c
+++ Lab1/task1.c
@@ -1,6 +1,45 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-int main() {
-    printf("Hello World");
+int main(int argc, char *argv[])
+{
+    int fd[2];
+    pid_t pid;
+    char buf[100];
+    int status;
+
+    if (pipe(fd) == -1) {
+        perror("pipe");
+        exit(1);
+    }
+
+    pid = fork();
+    if (pid == -1) {
+        perror("fork");
+        exit(1);
+    }
+
+    if (pid == 0) {
+        // child
+        close(fd[0]);
+        dup2(fd[1], STDOUT_FILENO);
+        close(fd[1]);
+        execlp("ls", "ls", "-l", NULL);
+        perror("execlp");
+        exit(1);
+    } else {
+        // parent
+        close(fd[1]);
+        dup2(fd[0], STDIN_FILENO);
+        close(fd[0]);
+        execlp("wc", "wc", "-l", NULL);
+        perror("execlp");
+        exit(1);
+    }
+
     return 0;
 }
